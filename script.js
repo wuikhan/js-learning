1b80d088b059e95833924ecabfae506f63626064
wuikhan <wuikhan@gmail.com>
2020-04-25 13:48:35 -0400

    made the changes to the index and js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('hello !!!')
+var fName = "John";
+console.log(fName);
+
+var lName = "Doe";
+var age = 29;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job; // it will print undefined because the value is not initialized
+console.log(job);
+
+job = "Programmer";
+console.log(job);
+
+/*
+ Variable mutation and type coercion
+*/
+
+var fName = 'John';
+var age = 29;
+console.log(fName + ' ' +age);
