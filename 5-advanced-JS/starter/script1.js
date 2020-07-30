9eeea224d89ba0d33e826905d873040830e44eea
Waqas Khan <me@waqaskhan.dev>
2020-07-29 21:46:25 -0400

    made some changes

--- 5-advanced-JS/starter/script1.js
+++ 5-advanced-JS/starter/script1.js
@@ -1,21 +1,21 @@
 // object.create
 
 var PersonProto = {
-    calculateAge: function () {
-        console.log(2016 - this.yearOfBirth);
-    }
+  calculateAge: function () {
+    console.log(2016 - this.yearOfBirth);
+  },
 };
 // not an ideal way
 var john = Object.create(PersonProto);
-john.name = 'John';
+john.name = "John";
 john.age = 1990;
-john.job = 'Analyst';
+john.job = "Analyst";
 
 var jane = Object.create(PersonProto, {
-    name: { value: 'Jane' },
-    age: { value: 1965 },
-    job: { value: 'Designer' }
-})
+  name: { value: "Jane" },
+  age: { value: 1965 },
+  job: { value: "Designer" },
+});
 
 // Primitive vs Objects
 //Primitive
@@ -29,8 +29,8 @@ console.log(b);
 // Objects
 
 var obj1 = {
-    name: 'John',
-    age: 26
+  name: "John",
+  age: 26,
 };
 
 var obj2 = obj1;
@@ -41,13 +41,13 @@ console.log(obj2.age);
 // Functions
 var age = 27;
 var obj = {
-    name: 'John',
-    city: 'Lisbon'
+  name: "John",
+  city: "Lisbon",
 };
 
 function change(a, b) {
-    a = 30;
-    b.city = 'San Francisco';
+  a = 30;
+  b.city = "San Francisco";
 }
 
 change(age, obj);
@@ -58,17 +58,24 @@ console.log(obj.city);
 // Passing functions as arguments
 
 var years = [1990, 1965, 1937, 2005, 1998];
-function arrayCalc(arr, fn){
-    var arrRes = [];
-    for(var i=0; i < arr.length; i++) {
-        arrRes.push(fn(arr[i]));
-    }
-    return arrRes;
+
+function arrayCalc(arr, fn) {
+  var arrRes = [];
+  for (var i = 0; i < arr.length; i++) {
+    arrRes.push(fn(arr[i]));
+  }
+  return arrRes;
 }
 
-function calculateAge(el){
-    return 2016 - el;
+function isFullAge(el) {
+  return el >= 18;
+}
+
+function calculateAge(el) {
+  return 2016 - el;
 }
 
 var ages = arrayCalc(years, calculateAge);
+var fullAges = arrayCalc(ages, isFullAge);
 console.log(ages);
+console.log(fullAges);
