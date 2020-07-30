46c2f1922865b3219e16b0b7a06508f186c73806
Waqas Khan <me@waqaskhan.dev>
2020-07-29 22:07:21 -0400

    functions returning function

--- 5-advanced-JS/starter/script1.js
+++ 5-advanced-JS/starter/script1.js
@@ -75,7 +75,39 @@ function calculateAge(el) {
   return 2016 - el;
 }
 
+function maxHearRate(el){
+
+if(el >=18 && el <=81){
+    return Math.round( 206.9 - (0.67 * el));
+} else {
+    return -1
+}
+}
+
 var ages = arrayCalc(years, calculateAge);
 var fullAges = arrayCalc(ages, isFullAge);
+var rates = arrayCalc(ages, maxHearRate);
+console.log(rates);
 console.log(ages);
 console.log(fullAges);
+
+
+// Functions returning functions
+function interviewQuestion(job){
+    if(job === 'designer'){
+        return function(name){
+            console.log(name + ', can you please explain what UX design is ?');
+        }
+    } else if(job === 'teacher') {
+        return function(name) {
+            console.log('What subject do you teach, ' +name + '?');
+        }
+    } else {
+        return function(name){
+            console.log('Hello ' +name+ ', what do you do ?');
+        }
+    }
+}
+
+var teacherQuestion = interviewQuestion('teacher');
+teacherQuestion('John');
